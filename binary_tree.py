537c1746e9ed0dd58336eea9481801a9ea143e2b
AlexanderGoose <alexandergoose97@gmail.com>
2024-08-22 20:53:18 -0600

    added bst

---
+++ binary_tree.py
@@ -0,0 +1,60 @@
+import random
+
+#######################################
+# .______        _______.___________. #
+# |   _  \      /       |           | #
+# |  |_)  |    |   (----`---|  |----` #
+# |   _  <      \   \       |  |      #
+# |  |_)  | .----)   |      |  |      #
+# |______/  |_______/       |__|      #
+#                                     #
+#######################################
+
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+        
+class Tree:
+    def __init__(self, root=None):
+        self.root = root
+
+    def insert(self, val=0):
+        if self.root is None:
+            self.root = Node(val)
+            return
+        else:
+            self.insert_rec(val, self.root)
+
+    def insert_rec(self, val, root):
+        if val < root.val:
+            if root.left is not None:
+                self.insert_rec(val, root.left)
+            else:
+                root.left = Node(val)
+        elif val > root.val:
+            if root.right is not None:
+                self.insert_rec(val, root.right)
+            else:
+                root.right = Node(val)
+
+    def inorder(self, root):
+        if root is not None:
+            self.inorder(root.left)     
+            print(root.val)
+            self.inorder(root.right)   
+
+if __name__ == "__main__":
+
+    tree = Tree()
+
+    num_list = [num for num in range(1, 7)]
+    random.shuffle(num_list)
+    print(num_list)
+
+    for num in num_list:
+        tree.insert(num)
+
+    tree.inorder(tree.root)
